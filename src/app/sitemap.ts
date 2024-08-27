058f440bf5ecbd4557edcc22c605d84aea75032a
Andrew Loloy <druloloy>
2024-08-27 20:53:43 +0800

    added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import type { MetadataRoute } from 'next'
+
+const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { docs: pages } = await fetch(`${serverURL}/api/pages?limit=0`).then(res => {
+    return res.json()
+  })
+  const { docs: projects } = await fetch(`${serverURL}/api/projects?limit=0`).then(res => {
+    return res.json()
+  })
+
+  const siteMap: MetadataRoute.Sitemap = []
+
+  for (const page of pages) {
+    siteMap.push({
+      url: `${serverURL}/${page.slug === 'home' ? '' : page.slug}`,
+      lastModified: page.updatedAt,
+      priority: 1,
+      changeFrequency: 'monthly',
+    })
+  }
+
+  for (const project of projects) {
+    siteMap.push({
+      url: `${serverURL}/projects/${project.slug}`,
+      lastModified: project.updatedAt,
+      priority: 0.8,
+      changeFrequency: 'monthly',
+    })
+  }
+
+  return siteMap
+}
