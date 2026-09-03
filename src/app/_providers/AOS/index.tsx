c6aaab6835a784c1b3b9612f3d52f081581f5c6a
druloloy <dru.loloy@gmail.com>
2026-09-03 19:50:48 +0800

    perf: drop unused gsap and ScrollTrigger from root layout

--- src/app/_providers/AOS/index.tsx
+++ src/app/_providers/AOS/index.tsx
@@ -1,10 +1,6 @@
 'use client'
 import React, { Fragment } from 'react'
 import AOS from 'aos'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
 
 import 'aos/dist/aos.css'
 
